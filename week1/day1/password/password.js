e1c002f654bb036af0f93b136022b44319c85b10
Todd Ferguson <toddsferg@gmail.com>
2016-10-04 17:52:23 +0000

    Lighthouse Stuff

---
+++ week1/day1/password/password.js
@@ -0,0 +1,11 @@
+
+var args = process.argv;
+
+
+
+
+
+
+
+
+
---
+++ week1/day2/adder.js
@@ -0,0 +1,19 @@
+
+
+var numbers = process.argv.slice(2);
+
+
+
+var total = 0;
+function sum(numbs){
+for(var i = 0; i < numbers.length; i++) {
+
+  var result = Number(numbers[i]);
+  total += result;
+  if( isNaN(number) === false) {
+    total += result
+
+  } else {console.log("stop giving me junk")
+}return total
+}
+console.log(sum(numbers));
